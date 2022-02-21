f7973b800f54725a709e992cba97210c56a830d1
V1Rka <59617994+V1Rka@users.noreply.github.com>
2022-02-21 20:04:23 +0500

    О компании

--- js/slider.js
+++ js/slider.js
@@ -18,3 +18,17 @@ var swiper2 = new Swiper(".my-swiper2", {
 	slidesPerGroup: 4,
 	spaceBetween: 30,
 });
+
+var swiper3 = new Swiper(".my-swiper3", {
+	navigation: {
+		nextEl: ".swiper-next3",
+		prevEl: ".swiper-prev3",
+	},
+	pagination: {
+		el: ".swiper-pagination2",
+		clickable: true,
+	},
+	slidesPerView: 4,
+	slidesPerGroup: 4,
+	spaceBetween: 30,
+});
