76f7afa04e516d7c641197d0423977f3939d339a
V1Rka <59617994+V1Rka@users.noreply.github.com>
2022-02-21 17:33:29 +0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,15 +1,21 @@
 let fields = document.querySelectorAll('.field__file');
+
 Array.prototype.forEach.call(fields, function (input) {
 	let label = input.nextElementSibling,
 		labelVal = label.querySelector('.field__file-fake').innerText;
 
 	input.addEventListener('change', function (e) {
-		let countFiles = '';
-		if (this.files && this.files.length >= 1)
-			countFiles = this.files.length;
+		let name = '',
+			countName = this.files[0].name.length,
+			difference = '';
 
-		if (countFiles)
-			label.querySelector('.field__file-fake').innerText = 'Выбрано файлов: ' + countFiles;
+		if (countName > 13) {
+			difference = countName - 13;
+			name = this.files[0].name.slice(0, -difference);
+		}
+
+		if (name)
+			label.querySelector('.field__file-fake').innerText = name + '......';
 		else
 			label.querySelector('.field__file-fake').innerText = labelVal;
 	});
